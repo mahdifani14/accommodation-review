7527862f8c2ceea07a645ab141930af876ac1d1b
Mahdi Fani-Disfani <mahdifani1369@gmail.com>
2018-01-15 19:21:09 +0100

    Add: constant variables

---
+++ server/constants.js
@@ -0,0 +1,34 @@
+'use strict';
+
+exports.USER_TYPE = {
+    ADMIN: 'admin'
+};
+
+exports.RATING_ASPECTS = [
+    'location',
+    'service',
+    'priceQuality',
+    'food',
+    'room',
+    'childFriendly',
+    'interior',
+    'size',
+    'activities',
+    'restaurants',
+    'sanitaryState',
+    'accessibility',
+    'nightlife',
+    'culture',
+    'surrounding',
+    'atmosphere',
+    'noviceSkiArea',
+    'advancedSkiArea',
+    'apresSki',
+    'beach',
+    'entertainment',
+    'environmental',
+    'pool',
+    'terrace'
+];
+
+exports.TRAVELED_WITH = ['SINGLE', 'FAMILY', 'COUPLE', 'FRIENDS', 'OTHER'];
