72862a81fe537943b17c2e2fd9bbf63893f922cc
Mahdi Fani-Disfani <mahdifani1369@gmail.com>
2018-01-16 10:09:08 +0100

    Add: reviews list page

---
+++ frontend/client/modules/Reviews/ReviewsListPage.js
@@ -0,0 +1,98 @@
+import React, { PropTypes, Component } from 'react';
+import { connect } from 'react-redux';
+import ImmutablePropTypes from 'react-immutable-proptypes';
+import { fetchReviews, fetchReviewsRatingAverages } from './ReviewsActions';
+import { getReviews, getReviewsRatingAverage, getReviewsTotal } from './ReviewsReducer';
+import ReviewsList from './components/ReviewsList/ReviewsList';
+import Search from './components/Search/Search';
+import { browserHistory } from 'react-router';
+import Pagination from '../Components/Pagination/Pagination';
+import config from '../../config';
+
+class ReviewsListPage extends Component {
+  static propTypes = {
+    reviews: ImmutablePropTypes.list.isRequired,
+    totalReviews: PropTypes.number.isRequired,
+    reviewsRatingAverages: PropTypes.object.isRequired,
+    fetchReviews: PropTypes.func.isRequired,
+    fetchReviewsRatingAverages: PropTypes.func.isRequired,
+    location: PropTypes.object.isRequired
+  };
+
+  state = {
+    pageNum: 0,
+  };
+
+  componentDidMount() {
+    this.props.fetchReviewsRatingAverages();
+    const perPage = config.reviewsPerPage;
+    this.props.fetchReviews(
+      this.props.location.query,
+      perPage,
+      0
+    );
+  }
+
+  componentWillReceiveProps(nextProps) {
+    const perPage = config.reviewsPerPage;
+    const shouldUpdate = nextProps.location.query.search !== this.props.location.query.search;
+    if (shouldUpdate) {
+      nextProps.fetchReviews(
+        nextProps.location.query,
+        perPage,
+        0
+      );
+    }
+  }
+
+  handlePageClick = (data) => {
+    const perPage = config.reviewsPerPage;
+    const selected = data.selected;
+    this.setState({ pageNum: selected }, () => {
+      this.props.fetchReviews(
+        this.props.location.query,
+        perPage,
+        selected
+      );
+    });
+  };
+
+  handleSearchSubmit = (formData) => {
+    browserHistory.push({
+      pathname: this.props.location.pathname,
+      query: {
+        search: formData.search
+      }
+    });
+  };
+
+  render() {
+    const { totalReviews, reviews } = this.props;
+    const totalsPage = Math.ceil(totalReviews / config.reviewsPerPage);
+
+    return (
+      <div>
+        <Search onSubmit={this.handleSearchSubmit}/>
+        <ReviewsList reviews={reviews}/>
+        <Pagination pageClick={this.handlePageClick} total={totalsPage}/>
+      </div>
+    );
+  }
+}
+
+function mapStateToProps(state, ownProps) {
+  const location = ownProps.location;
+  return {
+    reviews: getReviews(state),
+    totalReviews: getReviewsTotal(state),
+    reviewsRatingAverages: getReviewsRatingAverage(state),
+    location
+  };
+}
+
+const actions = {
+  fetchReviews,
+  fetchReviewsRatingAverages
+};
+
+export default connect(mapStateToProps, actions)(ReviewsListPage);
